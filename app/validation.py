6ae56793d05a5f69575e75f3b3a1b4a5cb272510
UR <ur@ForUsAlls-MacBook-Air.local>
2025-02-15 12:24:16 -0500

    Subiendo cambios al nuevo repo

--- app/validation.py
+++ app/validation.py
@@ -11,29 +11,38 @@ def normalize_input(data):
 # valido el email
 def validate_email(email):
     email = normalize_input(email)
-    return True
+    patron = r'^[A-Za-z0-9._%+-]+@urosario\.edu\.co$'
+    return bool(re.fullmatch(patron, email))
 
 
 # valido la edad
 def validate_dob(dob):
-
-    return True
+    try:
+        fecha = datetime.strptime(dob, "%Y-%m-%d")
+        edad_minima = 16
+        fecha_limite = datetime.now().replace(year=datetime.now().year - edad_minima)
+        return fecha <= fecha_limite
+    except ValueError:
+        return False
 
 
 # valido el usuario
 def validate_user(user):
-    return True
+    patron = r'^[A-Za-z]+(?:\.[A-Za-z]+)*$'
+    return bool(re.fullmatch(patron, user))
 
 
 # valido el dni
 def validate_dni(dni):
-    
-    return True
+    patron = r'^1\d{9}$' 
+    return bool(re.fullmatch(patron, dni))
 
 
 # valido la contraseña
 def validate_pswd(pswd):
-    return True
+    patron = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#*@\$%&\-!+=?])[A-Za-z\d#*@\$%&\-!+=?]{8,35}$'
+    return bool(re.fullmatch(patron, pswd))
+
 
 
 def validate_name(name):
