d42ef5a4e743a38bd37540f45c0e8e45fdb7020f
Prestamo Estudiantes <prestamour@urosario.edu.co>
2025-03-01 11:21:34 -0500

    Agregar archivos desde otro repositorio

--- app/encryption.py
+++ app/encryption.py
@@ -1,38 +1,68 @@
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 from Crypto.Hash import SHA256
 
 
 def hash_with_salt(texto):
-    texto_bytes = texto.encode()
+    # Generar un salt aleatorio
     salt = get_random_bytes(16)
-    texto_salt = texto_bytes + salt
-    hash = SHA256.new(texto_salt)
-    return hash.hexdigest(), salt.hex()
 
+    # Convertir el texto en claro a bytes
+    texto_bytes = texto.encode('utf-8')
 
-def compare_hashes(texto, stored_hash, salt):
-    texto_bytes = texto.encode()
-    salt = bytes.fromhex(salt)
-    texto_salt = texto_bytes + salt
-    computed_hash = SHA256.new(texto_salt).hexdigest()  # Usar otro nombre para el hash calculado
-    return computed_hash == stored_hash  # Comparar con el hash almacenado
+    # Crear un objeto de hash SHA-256
+    hash_obj = SHA256.new()
 
+    # Agregar la sal y el texto plano al hash
+    hash_obj.update(salt)
+    hash_obj.update(texto_bytes)
+
+    # Calcular el hash final
+    hash_result = hash_obj.digest()
+
+    # Devolver el hash
+    return hash_result.hex(), salt.hex()
 
 
 def decrypt_aes(texto_cifrado_str, nonce_str, clave):
-    texto_cifrado_bytes = bytes.fromhex(texto_cifrado_str)
-    nonce_bytes = bytes.fromhex(nonce_str)
-    cipher = AES.new(clave, AES.MODE_EAX, nonce=nonce_bytes)
-    texto_bytes = cipher.decrypt(texto_cifrado_bytes)
-    texto = texto_bytes.decode()
-    return texto
+    # Convertir el texto cifrado y el nonce de cadena de texto a bytes
+    texto_cifrado = bytes.fromhex(texto_cifrado_str)
+    nonce = bytes.fromhex(nonce_str)
+
+    # Crear un objeto AES con la clave y el nonce proporcionados
+    cipher = AES.new(clave, AES.MODE_EAX, nonce=nonce)
+
+    # Descifrar el texto
+    texto_descifrado = cipher.decrypt(texto_cifrado)
+
+    # Convertir los bytes del texto descifrado a una cadena de texto
+    return texto_descifrado.decode()
+
+
+def compare_salt(text, hash, salt):
+    salt = bytes.fromhex(salt)
+    # Convertir el texto en claro a bytes
+    texto_bytes = text.encode('utf-8')
+    # Crear un objeto de hash SHA-256
+    hash_obj = SHA256.new()
+    # Agregar la sal y el texto plano al hash
+    hash_obj.update(salt)
+    hash_obj.update(texto_bytes)
+    # Calcular el hash final
+    hash_result = hash_obj.digest()
+    # convertimos a hex
+    final = hash_result.hex()
+    print("final" + final)
+    if final == hash:
+        return True
+    else:
+        return False
 
 
 def encrypt_aes(texto, clave):
     # Convertir el texto a bytes
     texto_bytes = texto.encode()
-
 
     # Crear un objeto AES con la clave proporcionada
     cipher = AES.new(clave, AES.MODE_EAX)
@@ -46,3 +76,21 @@ def encrypt_aes(texto, clave):
 
     # Devolver el texto cifrado y el nonce
     return texto_cifrado_str, nonce.hex()
+
+# Función para ofuscar el DNI
+def ofuscar_dni(dni):
+    return '*' * (len(dni) - 4) + dni[-4:]
+
+if __name__ == '__main__':
+    texto = "Hola Mundo"
+    clave = get_random_bytes(16)
+    texto_cifrado, nonce = encrypt_aes(texto, clave)
+    print("Texto cifrado: " + texto_cifrado)
+    print("Nonce: " + nonce)
+    des = decrypt_aes(texto_cifrado, nonce, clave)
+    print("Texto descifrado: " + des)
+    password = "password"
+    pwd_hash = hash_with_salt(password)
+    print("Hash: " + pwd_hash[0])
+    password_other = "password"
+    print("Hash: " + hash_with_salt(password_other)[0])
--- app/validation.py
+++ app/validation.py
@@ -6,49 +6,48 @@ import unicodedata
 
 def normalize_input(data):
     if isinstance(data, str):
-        return unicodedata.normalize("NFKD", data)
+        # Normalizar el texto a la forma canónica
+        data = unicodedata.normalize('NFKD', data)
+        # Convertir a minúsculas y eliminar espacios en blanco
+        data = data.strip().lower()
     return data
 
 
 # valido el email
 def validate_email(email):
     email = normalize_input(email)
-    patron = r'^[A-Za-z0-9._%+-]+@urosario\.edu\.co$'
-    return bool(re.fullmatch(patron, email))
+    pattern = r'^[a-zA-Z0-9._%+-]+@urosario\.edu\.co$'
+    return re.match(pattern, email) is not None
 
 
 # valido la edad
 def validate_dob(dob):
-    try:
-        fecha = datetime.strptime(dob, "%Y-%m-%d")
-        edad_minima = 16
-        fecha_limite = datetime.now().replace(year=datetime.now().year - edad_minima)
-        return fecha <= fecha_limite
-    except ValueError:
-        return False
+    birth_date = datetime.strptime(dob, "%Y-%m-%d")
+    today = datetime.today()
+    age = today.year - birth_date.year
+
+    if (today.month, today.day) < (birth_date.month, birth_date.day):
+        age -= 1
+
+    return age >= 18
 
 
 # valido el usuario
 def validate_user(user):
-    patron = r'^[A-Za-z]+(?:\.[A-Za-z]+)*$'
+    patron = r'^[a-zA-Z]+\.[a-zA-Z]+$'
     return bool(re.fullmatch(patron, user))
 
 
 # valido el dni
 def validate_dni(dni):
-    patron = r'^1\d{9}$' 
+    patron = r'^\d{10}$'
     return bool(re.fullmatch(patron, dni))
 
 
 # valido la contraseña
 def validate_pswd(pswd):
-    patron = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#*@\$%&\-!+=?])[A-Za-z\d#*@\$%&\-!+=?]{8,35}$'
-    return bool(re.fullmatch(patron, pswd))
-
-
+    pattern = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#*@\$%&\-!+=?])[A-Za-z\d#*@\$%&\-!+=?]{8,35}$'
+    return bool(re.fullmatch(pattern, pswd))
 
 def validate_name(name):
-    if isinstance(name, str):
-        patron = r'^[A-Za-zÁÉÍÓÚáéíóúÑñÜü\s\']+$'
-        return bool(re.fullmatch(patron, name))
-    return False 
+    return bool(re.fullmatch(r'^[a-zA-Z]+$', name))
